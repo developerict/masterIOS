5292940dcd59eab1776b6411303e97e6d9691237
Innova Cloud Technology <innovacloudtechnology@mac-mini-de-innova.local>
2016-03-07 16:35:52 +0100

    Nuevo comió

---
+++ T1E1/NuevoviewController.swift
@@ -0,0 +1,35 @@
+//
+//  NuevoviewController.swift
+//  T1E1
+//
+//  Created by Innova Cloud Technology on 7/3/16.
+//  Copyright © 2016 developer ict. All rights reserved.
+//
+
+import UIKit
+
+class NuevoviewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            print("Nuevo viewdidload")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
